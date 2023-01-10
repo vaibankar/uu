e3e6c84d9f980de08cae7ca7829c179674550743
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-10 13:00:54 +0530

    ip

--- var.tf
+++ var.tf
@@ -1,4 +1,6 @@
 variable "user" {
-  type= list
-  default = [ "demo1","demo2","demo3","demo4","demo5" ]
+  type= list(string){
+    [ "demo1","demo2","demo3","demo4","demo5" ]
+  }
+  default = "demo"
 }
