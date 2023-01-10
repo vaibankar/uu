34974b2d53991ea57f54c330b244759f6237b7eb
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-10 12:19:24 +0530

    index.php

--- main.tf
+++ main.tf
@@ -2,14 +2,7 @@ provider "aws" {
   profile = "uu"
   region = "us-east-1"
 }
-
-resource "aws_instance" "my-instance" {
-  count         = var.instance_count
-  ami           = lookup(var.ami,var.aws_region)
-  instance_type = var.instance_type
-
-  tags = {
-    Name  = "Terraform-${count.index + 1}"
-    Batch = "PK"
-  }
+resource "aws_iam_user" "rs1" {
+  name = var.user
+  
 }
--- var.tf
+++ var.tf
@@ -1,20 +1,4 @@
-variable "ami" {
-  type = map
-
-  default = {
-    "us-east-1" = "ami-0574da719dca65348"
-    "us-east-1" = "ami-0a6b2839d44d781b2"
-  }
-}
-
-variable "instance_count" {
-  default = "5"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "aws_region" {
-  default = "us-east-1"
+variable "user" {
+  type= string
+  default="vaibhav"
 }
