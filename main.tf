aea179a5bd79594c4611c5d461afd3b5d40cbabe
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-10 13:19:40 +0530

    ip

--- main.tf
+++ main.tf
@@ -3,5 +3,16 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_iam_user" "rs1" {
-  name = var.user
+  name = var.userone
+}
+resource "aws_iam_user" "rs2" {
+  name = var.usertwo
+  
+}
+resource "aws_iam_user" "rs3" {
+  name = var.userthree
+  
+}
+resource "aws_iam_user" "rs4" {
+  name = var.userfour
 }
--- var.tf
+++ var.tf
@@ -1,4 +1,16 @@
-variable "user" {
+variable "userone" {
   type= string
   default = "userone"
+  usertwo={
+    type= string
+    default="usertwo"
+  }
+  userthree={
+    type=string
+    default="userthree"
+  }
+  userfour={
+    type=string
+    default="userfour"
+  }
 }
