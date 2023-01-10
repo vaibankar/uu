5708af5a1c60162b0194d7f95e146570d80ad245
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-10 13:49:30 +0530

    ip

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_iam_user" "rs1" {
-  name = var.userone
+name = var.userone
 }
 resource "aws_iam_user" "rs2" {
   name = var.u2
   
 }
 resource "aws_iam_user" "rs3" {
-  name = var.u3
+nam = var.u3
   
 }
 resource "aws_iam_user" "rs4" {
