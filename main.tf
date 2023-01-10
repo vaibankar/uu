f3d90899c6afff0a1969dc81d99eb373d96ebdf8
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-10 13:36:32 +0530

    ip

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ resource "aws_iam_group" "rs5" {
 resource "aws_iam_group_membership" "grup_mem" {
   name = "group ahe ha"
   users = [aws_iam_user.rs1.name,aws_iam_user.rs2.name,aws_iam_user.rs3.name,aws_iam_user.rs4.name]
-  group = [aws_iam_group.rs5.name]
+  group = aws_iam_group.rs5.name
   
 }
