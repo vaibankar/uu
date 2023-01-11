ecd9ad1b09e5c2d33241abcaed08716892ccfa9f
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-11 13:24:22 +0530

    bucket

--- main.tf
+++ main.tf
@@ -3,26 +3,31 @@ provider "aws" {
   region = "us-east-1"
 
 }
-resource "aws_s3_bucket" "demo-bucket"{
-  bucket = "zhinga"
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = "${aws_iam_role.test_role.name}"
 }
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = "${aws_iam_role.test_role.id}"
 
-  resource "aws_s3_bucket_policy" "public_read_access" {
-  bucket = aws_s3_bucket.demo-bucket.id
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Action": [
+        "s3:*"
+      ],
       "Effect": "Allow",
-	  "Principal": "*",
-      "Action": [ "s3:*" ],
-      "Resource": [
-        "${aws_s3_bucket.demo-bucket.arn}",
-        "${aws_s3_bucket.demo-bucket.arn}/*"
-      ]
+      "Resource": "*"
     }
   ]
 }
 EOF
+}
+resource "aws_instance" "role-test" {
+  ami = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
 }
