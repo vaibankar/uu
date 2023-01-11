b0648185c6eeebca485ffbc355ac57c9f3783aac
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-11 11:57:13 +0530

    indes

--- main.tf
+++ main.tf
@@ -38,5 +38,6 @@ resource "aws_security_group" "ssh" {
 }
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = aws_security_group.sg.id
-  network_interface_id = aws_instance.instance.primary_network_interface_id
+  security_group_id    = aws_security_group.sg1.id
+  network_interface_id = aws_instance.my-instance.primary_network_interface_id
 }
