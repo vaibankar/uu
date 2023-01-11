acc4a96cb12fbcd27007c005aa4a6abdf756c22a
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-11 12:40:12 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   profile = "uu"
   region = "us-east-1"
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {}
+
 }
 # Crate a AWS VPC which contains the following
 #   - VPC
@@ -10,7 +9,8 @@ data "aws_availability_zones" "available" {}
 #   - Private subnet(s)
 #   - Internet Gateway
 #   - Routing table
-
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
 resource "aws_vpc" "testapp" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true # Internal domain name
