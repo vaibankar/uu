f1bed0036bcf865267db4fd44b4c7ae63e156d79
vaibhav <vaibhavmbankar111@gmail.com>
2023-01-11 13:02:50 +0530

    bucket

--- main.tf
+++ main.tf
@@ -3,77 +3,26 @@ provider "aws" {
   region = "us-east-1"
 
 }
-# Crate a AWS VPC which contains the following
-#   - VPC
-#   - Public subnet(s)
-#   - Private subnet(s)
-#   - Internet Gateway
-#   - Routing table
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {}
-resource "aws_vpc" "testapp" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true # Internal domain name
-  enable_dns_hostnames = true # Internal host name
-
-  tags = {
-    Name = "testapp-vpc"
-  }
+resource "aws_s3_bucket" "demo-bucket"{
+  bucket = "zhinga"
 }
 
-resource "aws_subnet" "testapp_public_subnet" {
-  # Number of public subnet is defined in vars
-  count = var.number_of_public_subnets
-
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = "10.0.${count.index + 2}.0/24"
-  vpc_id                  = aws_vpc.testapp.id
-  map_public_ip_on_launch = true # This makes the subnet public
-
-  tags = {
-    Name = "testapp-public-subnet-${count.index}"
-  }
+  resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.demo-bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+	  "Principal": "*",
+      "Action": [ "s3:*" ],
+      "Resource": [
+        "${aws_s3_bucket.demo-bucket.arn}",
+        "${aws_s3_bucket.demo-bucket.arn}/*"
+      ]
+    }
+  ]
 }
-
-resource "aws_subnet" "testapp_private_subnet" {
-  # Number of private subnet is defined in vars
-  count = var.number_of_private_subnets
-
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.testapp.id
-
-  tags = {
-    Name = "testapp-private-subnet-${count.index}"
-  }
-}
-
-resource "aws_internet_gateway" "testapp_internet_gateway" {
-  vpc_id = aws_vpc.testapp.id
-
-  tags = {
-    Name = "testapp-internet-gateway"
-  }
-}
-
-resource "aws_route_table" "testapp_route_table" {
-  vpc_id = aws_vpc.testapp.id
-
-  route {
-    # Associated subet can reach public internet
-    cidr_block = "0.0.0.0/0"
-
-    # Which internet gateway to use
-    gateway_id = aws_internet_gateway.testapp_internet_gateway.id
-  }
-
-  tags = {
-    Name = "testapp-public-custom-rtb"
-  }
-}
-
-resource "aws_route_table_association" "testapp-custom-rtb-public-subnet" {
-  count          = 2
-  route_table_id = aws_route_table.testapp_route_table.id
-  subnet_id      = aws_subnet.testapp_public_subnet.*.id[count.index]
+EOF
 }
--- var.tf
+++
@@ -1,7 +0,0 @@
-variable "number_of_public_subnets" {
-  default = 2
-}
-
-variable "number_of_private_subnets" {
-  default = 2
-}
